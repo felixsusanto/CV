ddcc2f10fb02ad4bba36f25108fe1209cfeaa8c5
felix_susanto <felix.susanto2000@gmail.com>
2020-11-02 23:02:13 +0800

    wip split model

---
+++ src/models/cv.ts
@@ -0,0 +1,85 @@
+export const expertiseData = [
+  {
+    title: "Languages",
+    content: "CSS, XHTML, SCSS, JavaScript, TypeScript",
+  },
+  {
+    title: "Tools",
+    content: "Git, Grunt, Webpack, Google Analytics",
+  },
+  {
+    title: "Platforms",
+    content: "HubSpot, Wordpress",
+  },
+  {
+    title: "Frameworks",
+    content: "React JS, Angular, Jekyll, NodeJS",
+  },
+  {
+    title: "Libraries / APIs",
+    content: "Lodash, jQuery, Redux, Saga, Bootstrap, GraphQL",
+  },
+  {
+    title: "Other",
+    content:
+      "Digital Illustration, Adobe Flash, Photoshop, Illustrator and InDesign",
+  },
+];
+
+export const experienceData = [
+  {
+    company: "DBS Bank",
+    roles: [
+      {
+        name: "Application Developer Specialist",
+        period: "August 2018 – Present",
+        location: "Singapore",
+        description:
+          "Building robust, scalable, high performance distributed systems that connect microservices with mobile, web and smart devices to offer consumer-facing apps a wide range of functionality.",
+      },
+    ],
+  },
+  {
+    company: "Onyx Island",
+    roles: [
+      {
+        name: "Front End Developer Team Lead",
+        period: "March 2017 – August 2018",
+        location: "Singapore",
+        description:
+          "Worked with project managers to understand client’s requirement and distribute tasks to the front-end team accordingly. Ensuring projects are completed within the stipulated timeline in a satisfactory manner.",
+      },
+      {
+        name: "Senior Web Application Developer",
+        period: "April 2016 – March 2017",
+        location: "Singapore",
+        description:
+          "Front-end development for web app projects and maintain existing ones.",
+      },
+    ],
+  },
+  {
+    company: "Wireless Data Services (Ceased operation in Singapore)",
+    roles: [
+      {
+        name: "Multimedia Designer",
+        period: "March 2010 – February 2016",
+        location: "Singapore",
+        description:
+          "Brand design & development, UI design and flash animation for device simulator. Worked with developers to prepare assets and mock ups pre-development, provide technical and practical support to increase efficiency and software knowledge & skills among co-workers.",
+      },
+    ],
+  },
+  {
+    company: "Creatifactory",
+    roles: [
+      {
+        name: "Creative Director",
+        period: "2005 – 2010",
+        location: "Jakarta, Indonesia",
+        description:
+          "Print and digital visual development for brand identity, packaging and illustration.",
+      },
+    ],
+  },
+];
